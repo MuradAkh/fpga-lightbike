702a22a853053027d266bebbad5b6616c533cd07
Howard Chen <edt.chen@mail.utoronto.ca>
2018-11-18 20:05:16 -0500

    Milestone 1 Finished, Address is unstable, fixed few bugs. More bugs need to be looked at:

--- main.v
+++ main.v
@@ -73,7 +73,14 @@ module main (
         reset_game,
         run_game,
 
-        6'b000000,
+        {
+            make_lut[9'h052],
+            make_lut[9'h05D],
+            make_lut[9'h033],
+            make_lut[9'h03B],
+            make_lut[9'h01C], 
+            make_lut[9'h01B]
+        },
 
         colour,
         x,
@@ -211,44 +218,163 @@ module game_data(
         P2 = 2'd2,
         PE = 2'd3,
         
-        P0_C = 6'b110100,
-        P1_C = 6'b001010,
-        P2_C = 6'b111011,
-        PE_C = 6'b010101,
+        P0_C = 6'b011111,
+        P1_C = 6'b001100,
+        P2_C = 6'b111000,
+        PE_C = 6'b000001,
+        // P0_C = 6'b110000,
+        // P1_C = 6'b001100,
+        // P2_C = 6'b000011,
+        // PE_C = 6'b000000,
 
         WIDTH = 7'd80,
-        HEIGHT = 6'd60;
+        HEIGHT = 6'd60,
+        
+        D_WIDTH = 8'd160,
+        D_HEIGHT = 7'd120,
+        
+        L_IDLE = 4'd0,
+        W_P0 = 4'd1,
+        W_P1 = 4'd2,
+        W_P2 = 4'd3,
+
+        M_P0 = 4'd4,
+        M_P1 = 4'd5,
+        M_P2 = 4'd6,
+
+        C_P0 = 4'd7,
+        C_P1 = 4'd8,
+        C_P2 = 4'd9,
+        C_P0W = 4'd10,
+        C_P1W = 4'd11,
+        C_P2W = 4'd12;        
 
     reg [2:0] p_state;
-    // assign round_finished = ^p_state && ~&p_state && ~|p_state; // round is finished when one player standing
-    assign round_finished = 1'b0;
+    assign round_finished = ^p_state && ~&p_state || ~|p_state; // round is finished when one or zero player standing
+    // assign round_finished = 1'b0;
 
     // reg [2:0] p_in_air;
     reg [6:0] p_pos_x [2:0];
     reg [5:0] p_pos_y [2:0];
 
-    assign LEDR = p_pos_x[0];
+    assign LEDR[7:5] = p_state;
     // 00 is up, 01 is right, 10 is down, 11 is left
     reg [1:0] p_dir [2:0];
 
-    //  per cell         x      y
-    reg [1:0] g_state [79:0][59:0];
+
+    // RAM 
+    // PORT a -> game logic
+    // PORT b -> draw logic
+    reg [12:0] address_a;
+    wire [12:0] address_b;
+
+    reg [1:0] data_a, data_b;
+
+    reg wren_a;
+    wire wren_b;
+
+    wire [1:0] q_a, q_b;
+
+    ram game_state(
+        address_a,
+        address_b,
+        CLOCK_50,
+        data_a,
+        data_b,
+        wren_a,
+        wren_b,
+        q_a,
+        q_b
+    );
 
 
     // counter to let the game wait 32 ticks until starting
     reg [4:0] disp_counter;
     assign disp_continue = disp_counter == 0;
 
-    always @(posedge game_clk)
+    reg [12:0] reset_counter;
+
+
+    reg [3:0] logic_state, next_logic_state;
+
+    always @(*)
+    begin: move_check_players_control
+        if(run_game) begin
+            wren_a = 1'b0;
+            
+            case(next_logic_state)
+                W_P0: begin
+                    wren_a = p_state[0];
+                    address_a = p_pos_x[0] + p_pos_y[0] * WIDTH;
+                    data_a = P0;
+                end
+                W_P1: begin
+                    wren_a = p_state[1];
+                    address_a = p_pos_x[1] + p_pos_y[1] * WIDTH;
+                    data_a = P1;
+                end
+                W_P2: begin
+                    wren_a = p_state[2];
+                    address_a = p_pos_x[2] + p_pos_y[2] * WIDTH;
+                    data_a = P2;
+                end
+                C_P0W: begin
+                    address_a = p_pos_x[0] + p_pos_y[0] * WIDTH;
+                end
+                C_P1W: begin
+                    address_a = p_pos_x[1] + p_pos_y[1] * WIDTH;
+                end
+                C_P2W: begin
+                    address_a = p_pos_x[2] + p_pos_y[2] * WIDTH;
+                end
+                default:;
+            endcase
+
+            case(logic_state)
+                L_IDLE: next_logic_state <= game_clk ? W_P0 : L_IDLE;
+                W_P0: next_logic_state <= W_P1;
+                W_P1: next_logic_state <= W_P2;
+                W_P2: next_logic_state <= M_P0;
+                M_P0: next_logic_state <= M_P1;
+                M_P1: next_logic_state <= M_P2;
+                M_P2: next_logic_state <= C_P0W;
+                C_P0W: next_logic_state <= C_P0;
+                C_P0: next_logic_state <= C_P1W;
+                C_P1W: next_logic_state <= C_P1;
+                C_P1: next_logic_state <= C_P2W;
+                C_P2W: next_logic_state <= C_P2;
+                C_P2: next_logic_state <= L_IDLE;
+            endcase
+        end else if(reset_game) begin
+            wren_a <= 1'b1;
+            address_a <= reset_counter;
+            data_a <= 2'b11;
+        end else begin end
+    end
+
+    always @(posedge game_clk) begin
+        if(reset_game) begin
+            p_dir[2] = 2'd1;
+            p_dir[1] = 2'd3;
+            p_dir[0] = 2'd0;
+            disp_counter <= 5'b11111;
+        end else if(run_game) begin
+            integer player;
+            for(player = 0; player < 3; player = player + 1) begin
+                case(turning_dir[player])
+                    2'b01: p_dir[player] = p_dir[player] + 2'b1;
+                    2'b10: p_dir[player] = p_dir[player] - 2'b1;
+                    default:;
+                endcase
+            end
+        end else begin
+            disp_counter = disp_counter - 1'b1;
+        end
+    end
+
+    always @(posedge CLOCK_50)
     begin: game_logic
         if(reset_game) begin
-            integer i, j;
-            for(i = 0; i < WIDTH; i = i + 1) begin
-                for(j = 0; j < HEIGHT; j = j + 1) begin
-                    g_state[i][j] <= PE;
-                end
-            end
-            disp_counter <= 5'b11111;
 
             p_state <= 3'b111;
             
@@ -260,18 +386,68 @@ module game_data(
             p_pos_y[1] <= 7'd35;
             p_pos_y[0] <= 7'd25;
 
-            p_dir[2] = 2'd0;
-            p_dir[1] = 2'd3;
-            p_dir[0] = 2'd2;
+            logic_state <= L_IDLE;
+
+            reset_counter <= reset_counter == 13'd0 ? WIDTH * HEIGHT - 1 : reset_counter - 1'b1;
         end
         else if(run_game) begin
+            case(logic_state)
+                M_P0: begin
+                    case(p_dir[0])
+                        2'b00: p_pos_y[0] = p_pos_y[0] - 1'b1;
+                        2'b01: p_pos_x[0] = p_pos_x[0] + 1'b1;
+                        2'b10: p_pos_y[0] = p_pos_y[0] + 1'b1;
+                        2'b11: p_pos_x[0] = p_pos_x[0] - 1'b1;
+                    endcase
+                end
+                M_P1: begin
+                    case(p_dir[1])
+                        2'b00: p_pos_y[1] = p_pos_y[1] - 1'b1;
+                        2'b01: p_pos_x[1] = p_pos_x[1] + 1'b1;
+                        2'b10: p_pos_y[1] = p_pos_y[1] + 1'b1;
+                        2'b11: p_pos_x[1] = p_pos_x[1] - 1'b1;
+                    endcase
+                end
+                M_P2: begin
+                    case(p_dir[2])
+                        2'b00: p_pos_y[2] = p_pos_y[2] - 1'b1;
+                        2'b01: p_pos_x[2] = p_pos_x[2] + 1'b1;
+                        2'b10: p_pos_y[2] = p_pos_y[2] + 1'b1;
+                        2'b11: p_pos_x[2] = p_pos_x[2] - 1'b1;
+                    endcase
+                end
+                C_P0: begin
+                    if(
+                        //q_a != PE ||
+                        p_pos_x[0] >= WIDTH ||
+                        p_pos_y[0] >= HEIGHT) p_state[0] = 1'b0;
+                end
+                C_P1: begin
+                    if(
+                        //q_a != PE || 
+                        p_pos_x[1] >= WIDTH ||
+                        p_pos_y[1] >= HEIGHT) p_state[1] = 1'b0;
+                end
+                C_P2: begin
+                    if(
+                        //q_a != PE || 
+                        p_pos_x[2] >= WIDTH ||
+                        p_pos_y[2] >= HEIGHT) p_state[2] = 1'b0;
+                end
+                default:;
+            endcase
+
+            logic_state <= next_logic_state;
+
+        end
+    end
             // all the run game logic, 
             // update player positions
             // then first clear the board if player is dead
 
             // player movement
-            integer player;
-            integer i;
+            // integer player;
+            // integer i;
 
             // for(player = 0; player < 3; player = player + 1) begin
 
@@ -308,15 +484,19 @@ module game_data(
             //         if(g_state[player][p_pos_x[player]][p_pos_y[player]] != PE) p_state[player] = 1'b0;
             //     end
             // end
-            for(player = 0; player < 3; player = player + 1) begin
-                if(p_state[player] == 1'b1) begin
-                    p_pos_x[player] <= p_pos_x[player] + 1'b1;
-                    p_pos_y[player] <= p_pos_y[player] + 1'b1;
-                end
-                if (p_state[player] == 1'b1) begin
-                    g_state[p_pos_x[player]][p_pos_y[player]] = player;
-                end
-            end
+
+            // for(player = 0; player < 3; player = player + 1) begin
+            //     if(p_state[player] == 1'b1) begin
+
+
+            //         p_pos_x[player] <= p_pos_x[player] + 1'b1;
+            //         p_pos_y[player] <= p_pos_y[player] + 1'b1;
+            //     end
+
+            //     if (p_state[player] == 1'b1) begin
+            //         g_state[p_pos_x[player]][p_pos_y[player]] = player;
+            //     end
+            // end
 
 
             // // clear board
@@ -328,32 +508,29 @@ module game_data(
             //         default: g_state[i % WIDTH][i / WIDTH] <= PE;
             //     endcase
             // end
-        end
-        else begin
-            // display state
-            disp_counter <= disp_counter - 1;
-        end
-    end
 
 
     reg [14:0] draw_counter;
+    assign address_b = (draw_counter % D_WIDTH) / 2'd2 + (draw_counter / D_WIDTH) / 2'd2 * WIDTH;
+    assign wren_b = 1'd0;
+
     always @(posedge CLOCK_50)
     begin: draw_logic
         if(!reset_n)
-            draw_counter <= WIDTH * HEIGHT - 1'b1;
+            draw_counter <= D_WIDTH * D_HEIGHT - 1'b1;
         else
-            draw_counter <= draw_counter == 15'd0 ? WIDTH * HEIGHT - 1 : draw_counter - 1; 
+            draw_counter <= draw_counter == 15'd0 ? D_WIDTH * D_HEIGHT - 1 : draw_counter - 1; 
     end
 
     assign plot = 1'b1;
 
     always @(*)
-    begin: plot_logic
+    begin: b_logic
 
-        x = draw_counter % WIDTH;
-        y = draw_counter / WIDTH;
+        x = draw_counter % D_WIDTH;
+        y = draw_counter / D_WIDTH;
         
-        case(g_state[x][y])
+        case(q_b)
             P0: colour <= P0_C;
             P1: colour <= P1_C;
             P2: colour <= P2_C;
